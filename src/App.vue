9857c17c84e082ec20838b5fb1bed218f64de7e5
qpyracuk <qpyracuk@gmail.com>
2023-09-04 09:24:07 +0300

    Доделал на работе

--- src/App.vue
+++ src/App.vue
@@ -1,264 +1,193 @@
 <template>
-	<div id="app">
-		<div style="margin-bottom: 10%">{{ filter }}</div>
-		<center>
-			<FilterMenu v-model="filter" :build="data" :visible="visible" />
-		</center>
-	</div>
+    <div id="app">
+        <div style="margin-bottom: 10%">{{ filter }}</div>
+        <center>
+            <FilterMenu v-model="filter" :build="data" :visible="visible" />
+        </center>
+    </div>
 </template>
 
 <script>
-	import FilterMenu from "./components/sliding-filter-menu/sliding-filter-menu.vue";
-	const data = {
-		type: "checkbox",
-		name: "name-for-radio",
-		default: "kfkfkf",
-		all: {
-			title: "test",
-		},
-		values: {
-			true: "kfkfkf",
-			false: "Неправда...",
-		},
-		items: [
-			{
-				field: "value1",
-				title: "Что-то очень длинное",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value2",
-				title: "Пункт 2",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value3",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value4",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value5",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value6",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value7",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value8",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value9",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value10",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value11",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value12",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value13",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value14",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-			{
-				field: "value15",
-				title: "value3",
-				style: "fill: red",
-				image: "",
-				scripe: "test.scrite.svg",
-			},
-		],
-	};
-	export default {
-		name: "App",
-		data: function () {
-			return {
-				data: data,
-				filter: {
-					value1: '',
-					value2: '',
-					value3: '',
-					value4: '',
-					value5: '',
-					value6: '',
-					value7: '',
-					value8: '',
-					value9: '',
-					value10: '',
-					value11: '',
-					value12: '',
-					value13: '',
-					value14: '',
-					value15: '',
-				},
-				visible: true,
-			};
-		},
-		components: {
-			FilterMenu,
-		},
-	};
+import FilterMenu from "./components/sliding-filter-menu/sliding-filter-menu.vue";
+const data = {
+    type: "checkbox",
+    default: "lalalalalal",
+    debug: true,
+    sprite: require("@/assets/sprite.svg"),
+    all: {
+        title: "All",
+    },
+    values: {
+        true: "lalalalalal",
+        false: false,
+    },
+    items: [
+        {
+            field: "value1.test",
+            sprite: "icon-power-button",
+            style: "fill: #5bca8d;"
+        },
+        {
+            field: "value2",
+            sprite: "icon-power-button",
+            style: "fill: #98A2BE;"
+        },
+        {
+            field: "value3",
+            sprite: "icon-snow",
+            style: "fill: #007fff;"
+        },
+        {
+            field: "value4",
+            sprite: "icon-water",
+            style: "fill: #fcca5f;"
+        },
+        {
+            field: "value5",
+            sprite: "icon-disconnect",
+            style: "fill: #fcca5f;",
+        },
+        {
+            field: "value6",
+            sprite: "icon-opened-door",
+            style: "fill: #FB6C6C;"
+        },
+        {
+            field: "value7",
+            sprite: "icon-triangle-with-exclaim",
+            style: "fill: #FB6C6C;"
+        },
+        {
+            title: "?",
+            field: "value8",
+            style: "fill: #98A2BE;"
+        },
+    ],
+};
+export default {
+    name: "App",
+    data: function () {
+        return {
+            data: data,
+            filter: {
+                value1: { test: "" },
+                value2: '',
+                value3: '',
+                value4: '',
+                value5: '',
+                value6: '',
+                value7: '',
+                value8: ''
+            },
+            visible: true,
+        };
+    },
+    components: {
+        FilterMenu,
+    },
+};
 </script>
 
 <style>
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
 
-	/*-------- Clear all basic CSS --------*/
+/*-------- Clear all basic CSS --------*/
 
-	* {
-		padding: 0;
-		margin: 0;
-		border: 0;
-	}
+* {
+    padding: 0;
+    margin: 0;
+    border: 0;
+}
 
-	*,
-	*:before,
-	*:after {
-		-moz-box-sizing: border-box;
-		-webkit-box-sizing: border-box;
-		box-sizing: border-box;
-	}
+*,
+*:before,
+*:after {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
 
-	:focus,
-	:active {
-		outline: none;
-	}
+:focus,
+:active {
+    outline: none;
+}
 
-	a:focus,
-	a:active {
-		outline: none;
-	}
+a:focus,
+a:active {
+    outline: none;
+}
 
-	nav,
-	footer,
-	header,
-	aside {
-		display: block;
-	}
+nav,
+footer,
+header,
+aside {
+    display: block;
+}
 
-	html,
-	body {
-		height: 100%;
-		width: 100%;
-		font-size: 100%;
-		line-height: 1;
-		font-size: 14px;
-		-ms-text-size-adjust: 100%;
-		-moz-text-size-adjust: 100%;
-		-webkit-text-size-adjust: 100%;
-	}
+html,
+body {
+    height: 100%;
+    width: 100%;
+    font-size: 100%;
+    line-height: 1;
+    font-size: 14px;
+    -ms-text-size-adjust: 100%;
+    -moz-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+}
 
-	input,
-	button,
-	textarea {
-		font-family: inherit;
-	}
+input,
+button,
+textarea {
+    font-family: inherit;
+}
 
-	input:-ms-clear {
-		display: none;
-	}
+input:-ms-clear {
+    display: none;
+}
 
-	button {
-		cursor: pointer;
-	}
+button {
+    cursor: pointer;
+}
 
-	button:-moz-focus-inner {
-		padding: 0;
-		border: 0;
-	}
+button:-moz-focus-inner {
+    padding: 0;
+    border: 0;
+}
 
-	a,
-	a:visited {
-		text-decoration: none;
-	}
+a,
+a:visited {
+    text-decoration: none;
+}
 
-	a:hover {
-		text-decoration: none;
-	}
+a:hover {
+    text-decoration: none;
+}
 
-	ul li {
-		list-style: none;
-	}
+ul li {
+    list-style: none;
+}
 
-	img {
-		vertical-align: top;
-	}
+img {
+    vertical-align: top;
+}
 
-	h1,
-	h2,
-	h3,
-	h4,
-	h5,
-	h6 {
-		font-size: inherit;
-		font-weight: 400;
-	}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-size: inherit;
+    font-weight: 400;
+}
 
-	/*--------------------*/
+/*--------------------*/
 </style>
--- src/components/sliding-filter-menu/modules/sliding-filter-menu.js
+++ src/components/sliding-filter-menu/modules/sliding-filter-menu.js
@@ -1,160 +1,160 @@
 export default {
-	name: "sliding-filter-menu",
-	props: {
-		visible: Boolean,
-		filter: Object,
-		build: Object,
-	},
+    name: "sliding-filter-menu",
+    props: {
+        visible: Boolean,
+        filter: Object,
+        build: Object,
+    },
 
-	data() {
-		return {
-			allValue: false,
-			errorFlag: false,
-		};
-	},
+    data() {
+        return {
+            allValue: false,
+            errorFlag: false,
+        };
+    },
 
-	model: {
-		prop: "filter",
-		event: "filterChangeEvent",
-	},
+    model: {
+        prop: "filter",
+        event: "filterChangeEvent",
+    },
 
-	methods: {
-		all: function () {
-			const len = this.build.items.length;
-			const items = this.build.items;
-			for (let i = 0; i < len; i++) this.getFieldPath(this.filter, items[i])[this.getLastField(items[i])] = this.allValue;
-		},
+    methods: {
+        all: function () {
+            const len = this.build.items.length;
+            const items = this.build.items;
+            for (let i = 0; i < len; i++) this.getFieldPath(this.filter, items[i])[this.getLastField(items[i])] = this.allValue;
+        },
 
-		change() {
-			const len = this.build.items.length;
-			const items = this.build.items;
-			let number = 0;
-			for (let i = 0; i < len; i++) {
-				if (this.getFieldPath(this.filter, items[i])[this.getLastField(items[i])] === this.build.values.true) number++;
-			}
-			if (number === len) this.allValue = this.build.values.true;
-			else this.allValue = this.build.values.false;
-		},
+        change() {
+            const len = this.build.items.length;
+            const items = this.build.items;
+            let number = 0;
+            for (let i = 0; i < len; i++) {
+                if (this.getFieldPath(this.filter, items[i])[this.getLastField(items[i])] === this.build.values.true) number++;
+            }
+            if (number === len) this.allValue = this.build.values.true;
+            else this.allValue = this.build.values.false;
+        },
 
-		getFieldPath(filter, item) {
-			let fields = item.field.split(".");
-			if (fields.length === 1) return filter;
-			let len = fields.length - 3;
-			let current = filter[fields[0]];
-			for (let i = 1; i < len; i++) current = current[fields[i]];
-			return current;
-		},
+        getFieldPath(filter, item) {
+            let fields = item.field.split(".");
+            if (fields.length === 1) return filter;
+            let len = fields.length - 3;
+            let current = filter[fields[0]];
+            for (let i = 1; i < len; i++) current = current[fields[i]];
+            return current;
+        },
 
-		getLastField(item) {
-			let fields = item.field.split(".");
-			return fields[fields.length - 1];
-		},
-		log(message) {
-			console.log(`%c[filter-menu] LOG\n| ${message} |`, "color: gray");
-		},
-		confirm(message) {
-			console.log(`%c[filter-menu] CONFIRM\n| ${message} |`, "color: green");
-		},
-		warn(message) {
-			this.errorFlag = true;
-			console.info(`%c[filter-menu] WARNNING\n${message}`, "color: yellow");
-		},
-		error(message) {
-			this.errorFlag = true;
-			console.error(`[filter-menu] ERROR\n${message}`);
-		},
-	},
+        getLastField(item) {
+            let fields = item.field.split(".");
+            return fields[fields.length - 1];
+        },
+        log(message) {
+            console.log(`%c[filter-menu] LOG\n| ${message} |`, "color: gray");
+        },
+        confirm(message) {
+            console.log(`%c[filter-menu] CONFIRM\n| ${message} |`, "color: green");
+        },
+        warn(message) {
+            this.errorFlag = true;
+            console.info(`%c[filter-menu] WARNNING\n${message}`, "color: yellow");
+        },
+        error(message) {
+            this.errorFlag = true;
+            console.error(`[filter-menu] ERROR\n${message}`);
+        },
+    },
 
-	created() {
-		const build = this.build;
-		if (build.values === undefined) build.values = {};
-		if (build.values.true === undefined) build.values.true = true;
-		if (build.values.false === undefined) build.values.false = false;
-		if (build.default === undefined) build.default = build.values.false;
-		if (build.all !== undefined && build.type !== "checkbox") build.all = undefined;
+    created() {
+        const build = this.build;
+        if (build.values === undefined) build.values = {};
+        if (build.values.true === undefined) build.values.true = true;
+        if (build.values.false === undefined) build.values.false = false;
+        if (build.default === undefined) build.default = build.values.false;
+        if (build.all !== undefined && build.type !== "checkbox") build.all = undefined;
+        if (build.debug) {
+            this.log("РЕЖИМ ОТЛАДКИ ВКЛЮЧЕН");
+            /**
+             * Проверка настроек для режима работы компонента
+             */
+            if (build.type !== undefined)
+                switch (build.type) {
+                    case "checkbox":
+                        this.log("Режим checkbox");
+                        break;
+                    case "radio":
+                        if (build.name === undefined) this.error("Для того, чтобы использовать режим radio, необходимо добавить поле name в объект конфигурации.");
+                        this.log("Режим radio");
+                        break;
+                    default:
+                        this.error("Неправильно указан режим работы (type = checkbox/radio\n");
+                        break;
+                }
+            else this.error("Не указан тип ввода");
 
-		if (build.debug) {
-			this.log("РЕЖИМ ОТЛАДКИ ВКЛЮЧЕН");
-			/**
-			 * Проверка настроек для режима работы компонента
-			 */
-			if (build.type !== undefined)
-				switch (build.type) {
-					case "checkbox":
-						this.log("Режим checkbox");
-						break;
-					case "radio":
-						if (build.name === undefined) this.error("Для того, чтобы использовать режим radio, необходимо добавить поле name в объект конфигурации.");
-						this.log("Режим radio");
-						break;
-					default:
-						this.error("Неправильно указан режим работы (type = checkbox/radio\n");
-						break;
-				}
-			else this.error("Не указан тип ввода");
+            /**
+             * Проверка настроек для элементов компонента
+             */
+            if (build.items === undefined) this.error("Список элементов отсутствует");
+            else if (Array.isArray(build.items)) {
+                if (build.items.length > 0) {
+                    let itemsFlag = true;
+                    for (let i = 0; i < build.items.length; i++) {
+                        let item = build.items[i];
+                        if (item.field === undefined) {
+                            itemsFlag = false;
+                            this.error(`Для поля под номером: ${i} отсутствует значение field\nЭто поле необходимо компоненту для того, чтобы реактивно изменять нужное поле в объекте фильтраци`);
+                        }
+                        if (this.getFieldPath(this.filter, item)[this.getLastField(item)] === undefined) {
+                            itemsFlag = false;
+                            this.error(`\nПоле ${item.field} в редактируемом объекте "Filter" не существует.\nСоздайте его, чтобы не потерять реактивность!`);
+                        }
+                    }
+                    if (itemsFlag) this.log("Пункты меню успешно инициализированы");
+                } else this.warn("Список элементов пуст");
+            } else this.error("Поле items должно быть массивом");
 
-			/**
-			 * Проверка настроек для элементов компонента
-			 */
-			if (build.items === undefined) this.error("Список элементов отсутствует");
-			else if (Array.isArray(build.items)) {
-				if (build.items.length > 0) {
-					let itemsFlag = true;
-					for (let i = 0; i < build.items.length; i++) {
-						let item = build.items[i];
-						if (item.field === undefined) {
-							itemsFlag = false;
-							this.error(`Для поля под номером: ${i} отсутствует значение field\nЭто поле необходимо компоненту для того, чтобы реактивно изменять нужное поле в объекте фильтраци`);
-						}
-						if (this.getFieldPath(this.filter, item)[this.getLastField(item)] === undefined) {
-							itemsFlag = false;
-							this.error(`\nПоле ${item.field} в редактируемом объекте "Filter" не существует.\nСоздайте его, чтобы не потерять реактивность!`);
-						}
-					}
-					if (itemsFlag) this.log("Пункты меню успешно инициализированы");
-				} else this.warn("Список элементов пуст");
-			} else this.error("Поле items должно быть массивом");
+            /**
+             * Проверка настроек для выбора всего
+             */
+            if (build.all !== undefined) {
+                if (build.type === "checkbox") {
+                    if (build.all.title !== undefined) {
+                        if (typeof build.all.title !== "string" && typeof build.all.title !== "number") {
+                            this.warn("Для название пункта ALL рекомендуется использовать string или number");
+                        } else {
+                            this.log(`Добавлен общий пункт "${build.all.title}"`);
+                        }
+                    } else {
+                        this.error("Название пункта для выбора всего отсутствует");
+                    }
+                } else {
+                    this.error('Для режима radio невозможно создать пункт "All"');
+                }
+            }
+            if (build.default !== undefined) {
+                if (build.type !== "checkbox") this.error("Значение по умолчанию доступно только для режима [checkbox]");
+                else if (build.default !== build.values.true && build.default !== build.values.false) this.error("Значение по умолчанию задано некорректно");
+            }
 
-			/**
-			 * Проверка настроек для выбора всего
-			 */
-			if (build.all !== undefined) {
-				if (build.type === "checkbox") {
-					if (build.all.title !== undefined) {
-						if (typeof build.all.title !== "string" && typeof build.all.title !== "number") {
-							this.warn("Для название пункта ALL рекомендуется использовать string или number");
-						} else {
-							this.log(`Добавлен общий пункт "${build.all.title}"`);
-						}
-					} else {
-						this.error("Название пункта для выбора всего отсутствует");
-					}
-				} else {
-					this.error('Для режима radio невозможно создать пункт "All"');
-				}
-			}
-			if (build.default !== undefined) {
-				if (build.type !== "checkbox") this.error("Значение по умолчанию доступно только для режима [checkbox]");
-				else if (build.default !== build.values.true && build.default !== build.values.false) this.error("Значение по умолчанию задано некорректно");
-			}
+            if (this.errorFlag) this.warn("Исправьте проблемы в объекте конфигурации");
+            else this.confirm("ВАЛИДАЦИЯ УСПЕШНО ПРОЙДЕНА!");
+        }
 
-			if (this.errorFlag) this.warn("Исправьте проблемы в объекте конфигурации");
-			else this.confirm("ВАЛИДАЦИЯ УСПЕШНО ПРОЙДЕНА!");
-		}
-
-		const len = build.items.length;
-		const items = build.items;
-		let number = 0;
-		for (let i = 0; i < len; i++) {
-			let field = this.getFieldPath(this.filter, items[i]);
-			let fieldName = this.getLastField(items[i]);
-			if (field[fieldName] === undefined) field[fieldName] = build.default;
-			if (field[fieldName] === build.values.true) number++;
-		}
-		console.log(number, len, build.values.false);
-		if (number === len) {
-			this.allValue = build.values.true;
-		} else this.allValue = build.values.false;
-		this.$emit("filterChangeEvent", this.filter);
-	},
+        const len = build.items.length;
+        const items = build.items;
+        let number = 0;
+        for (let i = 0; i < len; i++) {
+            let field = this.getFieldPath(this.filter, items[i]);
+            let fieldName = this.getLastField(items[i]);
+            field[fieldName] = build.default;
+            if (field[fieldName] === build.values.true) number++;
+        }
+        console.log(number, len, build.values.false);
+        if (number === len) {
+            this.allValue = build.values.true;
+        } else this.allValue = build.values.false;
+        this.all()
+        this.$emit("filterChangeEvent", this.filter);
+    },
 };
